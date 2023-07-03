2a72a399c41ae77969c61dafcdfbc09e9cd85f74
Dipeshxman11 <dipeshramane9378>
2023-07-04 01:02:33 +0530

    secondfile

---
+++ dom.js
@@ -0,0 +1,6 @@
+const headerTitle = document.getElementById("header-title");
+headerTitle.style.border = "1px solid black";
+
+const addItemTitle = document.getElementsByClassName("title")[0];
+addItemTitle.style.fontWeight = "bold";
+addItemTitle.style.color = "green";
