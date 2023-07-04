99ab279eea17f06be2e46e1812a59079608201db
Dipeshxman11 <dipeshramane9378>
2023-07-04 14:09:00 +0530

    first commit

--- demo.js
+++ demo.js
@@ -1,1 +1,7 @@
-console.log("welcome")
+var Items = document.getElementsByClassName("list-group-item");
+
+Items[3].style.backgroundColor="Green";
+
+  for (var i = 0; i < Items.length; i++) {
+    Items[i].style.fontWeight = "bold";
+  }
